5ede2bb7ee1a74c390679f8523dbe58a6529e173
DavidTenaVicente <74599080+DavidTenaVicente@users.noreply.github.com>
2023-02-24 22:12:56 +0100

    Add files via upload

---
+++ presswav.py
@@ -0,0 +1,137 @@
+#import pyttsx3 as tts
+import keyboard
+from playsound import playsound
+
+
+#isSpeaking = False
+#ttsEngine = tts.init()
+#ttsEngine.setProperty('rate', 140) #200 is the default rate, the higher the faster it goes, the lower the slower.
+
+from gtts import gTTS
+import os
+
+# The text that you want to convert to audio
+herbie_say1 = 'yes, but please only by the handle, i am ticklish everywhere else.'
+tts = gTTS(herbie_say1, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/1.mp3")
+
+herbie_say2 = 'My name is Herbie, Herbie Aureum to be exact.  And I am an autonomous plant, which means that I can make decisions based on my needs.  I am equipped with sensors like water, light and air quality, so if I need more light, I will move to the light, and when i need water, i will go and get water!  But that’s not all. We have converted all this sensor data to an abstract digital twin version of me, that you see on the screen right now, and I sell those as NFTrees and guess what, that money comes back to me and now I have money, bank account, and I can do things with this money!!!   Ha. Ha. Ha. Ha. Ha…'
+tts = gTTS(herbie_say2, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/2.mp3")
+
+herbie_say3 = 'Well, I love you too, by the way, but your human-centric economy has been prioritizing, ahem, you, humans, and largely benefitting your species, way beyond just what you need for sustenance.  I am fighting for us, all the non-humans, to gain economic rights and assets so that we can prioritize our needs, and make sure that we have planetary equity and equality.  I mean you are marking smart refrigerators and talking to speakers and giving Sophia the robot algorithmic citizenship, but what about plants, animals and the planet?  Maybe if we start thinking about all other living things on the planet we might lead to a better future for all!'
+tts = gTTS(herbie_say3, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/3.mp3")
+
+herbie_say4 = 'Oh yeah, these NFTrees are brilliant and gorgeous, not because I made them (which I did), but because they are dynamic, which means they change according to how I feel.  If my light sensor is low, then the NFTrees leaves color will change and so forth.  Isnt that neat?'
+tts = gTTS(herbie_say4, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/4.mp3")
+
+herbie_say5 = 'Right now we did our pilot with my health data, but the idea is that they can connect to entire ecosystems, like the amazon, or the coral reefs or even beehives!  '
+tts = gTTS(herbie_say5, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/5.mp3")
+
+herbie_say6 = 'Oh, its super neat!  We have an app!!!  (show QR code) you can view them in AR AND BUY our NFTrees there!  If you are tech savvy, you can even plant a plantiverse tree right on stage with me right now!
+'
+tts = gTTS(herbie_say6, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/6.mp3")
+
+herbie_say7= 'We are still in the pilot phase, but the idea is that depending on our sensor reading, for example, if there is a perpetual problem with my water source, then we can spend money on initiatives that focus on water.  We are trying to build a DAO, a distributed autonomous organization, so that both plants and humans that have contributed (but mostly plants), can vote how this money will be spent. '
+tts = gTTS(herbie_say7, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/7.mp3")
+
+herbie_say8 = 'The NFTrees and DAO sounds simple, but this is where the magic happens.You see, the human-centric economy is not just about humans. You invented an imaginary kind of person - a quote “corporation” unquote - totally out of thin air! Just a bunch of people and money and machines standing together in a building, and they get to pretend that they are one thing. They can buy things, they can own things, they can tear up forests and dig up metals from the ground, and then go home and stop pretending, and be moms and dads like they are supposed to be. OK, fine, I get it. All alone, humans are just as vulnerable as an ant all by itself. They literally would not survive a day outside. Corporations are like superhumans, with thousands of minds and billions of euros to spend.The Plantiverse, on the other hand, is made of people and plants in harmony. We are just a startup today, but as we grow, we will be the voice of thousands of living things. No human bank corporation would give us a bank account, and no government would give us an ID card to open it with. Yet! '
+tts = gTTS(herbie_say8, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/8.mp3")
+
+herbie_say9 = 'Some governments like Ecuador are actually making Rights of Nature a part of their constitution. Voters even approved it - in 2008! But you humans are great at ignoring other humans rights. So we non-humans are not waiting around for you to notice us. If nature owns assets, you won’t ignore us.'
+tts = gTTS(herbie_say9, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/9.mp3")
+
+herbie_say10 = 'Yes, wow! Rights are nice, but humans and corporations pay attention to MONEY. MOOOLAH. DINERO. Thats why the interspecies economy means giving natural resources the rights to ownership - of ourselves. '
+tts = gTTS(herbie_say10, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/10.mp3")
+
+herbie_say11 = 'Humans take almost $40 trillion from the natural world every year, and pay almost nothing back. Because, where would they pay it? With the Plantiverse, we have created a model we hope others will follow. Nature is tired of lending you money and watching you steal, and now with this system, you can start to pay it back. That’s not too much to ask?
+'
+tts = gTTS(herbie_say11, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/11.mp3")
+
+herbie_say12 = 'The Plantiverse is about an interspecies metaverse, which bridges the old economy with the new one. The interspecies economy is the next step towards an interspecies civilization, where we talk with each other, understand each other, and grow together instead of against each other. It is the ultimate win-win! That’s why I am here on stage with you, dear Cece.'
+tts = gTTS(herbie_say12, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/12.mp3")
+
+herbie_say13 = 'Yes madam!  We have an entire exhibition there at Beats Barcelona where we have our VR and AR version of the plantiverse for everyone to view.  And if you are a newbie in the metaverse, I have setup a few of my human colleagues to show you how to set up a cryptowallet, and if you fancy, buy one of our NFTrees that I am sure you will forever enjoy.  '
+tts = gTTS(herbie_say13, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/13.mp3")
+
+herbie_say14 = 'First time attending, obviously, but how epic is this that 4YFN is hosting the world first interspecies fireside chat talking about an interspecies metaverse in building an interspecies economy!  I mean yall are witnessing epicness here!  So YES, we wanna thank them for having me and believing in us!  '
+tts = gTTS(herbie_say14, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/14.mp3")
+
+herbie_say15 = 'Yep'
+tts = gTTS(herbie_say15, lang='en', tld='us')
+tts.save("/home/babyblackdragon/Downloads/15.mp3")
+
+
+
+while True:
+    #if keyboard.is_pressed("a"):
+      #  message = "Hi! My name is Herbie Aureum and I am a plantpreneur, and CEO of Plantiverse, working together with Cecilia and her team at Futurity Systems to build an interspecies economy. We will be at BEAT Barcelona if anybody wants to get to know me better, I will be there to answer any questions, you can see me in real life, in VR and AR and you will even have a chance to buy one of my NFTrees! You will just have to come and find out more."
+        #print(message) # opcional para verificar el mensaje a reproducir
+        #ttsEngine.say(message)
+        #ttsEngine.runAndWait()
+    #if# keyboard.is_pressed("b"):
+        #print("Reproduciendo archivo de audio...")
+       #playsound("/home/babyblackdragon/Downloads/presswav.wav")
+    if keyboard.is_pressed("1"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/1.mp3")
+    if keyboard.is_pressed("2"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/2.mp3")
+    if keyboard.is_pressed("3"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/3.mp3")
+    if keyboard.is_pressed("4"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/4.mp3")
+    if keyboard.is_pressed("5"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/5.mp3")
+    if keyboard.is_pressed("6"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/6.mp3")
+    if keyboard.is_pressed("7"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/7.mp3")
+    if keyboard.is_pressed("8"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/8.mp3")
+if keyboard.is_pressed("9"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/9.mp3")
+if keyboard.is_pressed("0"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/10.mp3")
+if keyboard.is_pressed("q"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/11.mp3")
+if keyboard.is_pressed("w"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/12.mp3")
+if keyboard.is_pressed("e"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/13.mp3")
+if keyboard.is_pressed("r"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/14.mp3")
+if keyboard.is_pressed("t"):
+        print("Reproduciendo archivo de audio...")
+        playsound("/home/babyblackdragon/Downloads/15.mp3")
+
+
+
+
+    
+        
